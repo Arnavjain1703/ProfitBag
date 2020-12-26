5196d2b175c051c42c9c2b4097c4066ac8485dc1
Rakshit <rakshitpanjwani@gmail.com>
2020-12-26 22:58:52 +0530

    added feedback section and carousel

--- frontend/src/Components/Features/Features.js
+++ frontend/src/Components/Features/Features.js
@@ -1,5 +1,8 @@
 import React, { Component } from "react";
 import classes from './Features.module.css';
+import {ReactComponent as DeviceIcon} from '../../assets/icons/devices.svg';
+import {ReactComponent as NotificationsIcon} from '../../assets/icons/notifications.svg';
+import {ReactComponent as TradeIcon} from '../../assets/icons/trade.svg';
 
 class Features extends Component {
 
@@ -7,16 +10,23 @@ class Features extends Component {
      
       return (
         <div className={classes.features}>
-          
+        <div className={classes.heading}> Features we provide</div>
+            
         <div className={classes.featureContainer}>
         <div className={classes.featurebox}>
-        hiii
+        <DeviceIcon className={classes.icon}/>
+        <p className={classes.featurehead}>Access on Any Platform</p>
+        <p className={classes.text}>Trade across web and mobile<br /> without skipping a beat.</p>
         </div>
         <div className={classes.featurebox}>
-        hiii
+        <NotificationsIcon className={classes.icon}/>
+        <p className={classes.featurehead}>Get Instant Notifications</p>
+        <p className={classes.text}>Keep track of your orders <br/> and other trading activity.</p>
         </div>
         <div className={classes.featurebox}>
-        hiii
+        <TradeIcon className={classes.icon}/>
+        <p className={classes.featurehead}>Trade on the GO!</p>
+        <p className={classes.text}>Trade anytime, even with <br/> slow internet connections.</p>
         </div>
         </div>
         </div>
--- frontend/src/Components/Feedback/Feedback.js
+++ frontend/src/Components/Feedback/Feedback.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import classes from './Feedback.module.css';
+import Carousel from 'react-bootstrap/Carousel' 
 
 class Feedback extends Component {
 
@@ -7,8 +8,128 @@ class Feedback extends Component {
      
       return (
         <div className={classes.feedback}>
-        feedback
-        </div>
+            <div className={classes.heading}>What do our clients say?</div>
+            
+            
+            {/* <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Rtrade <br/>
+            since Jan-2017. All I can <br/>
+            say that they are best in <br/>
+            terms of broker and <br/>
+            technology. And it is the<br/> 
+            smoothest app available <br/>
+            on any brokerage<br/> platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Rtrade <br/>
+            since Jan-2017. All I can <br/>
+            say that they are best in <br/>
+            terms of broker and <br/>
+            technology. And it is the<br/> 
+            smoothest app available <br/>
+            on any brokerage<br/> platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Rtrade <br/>
+            since Jan-2017. All I can <br/>
+            say that they are best in <br/>
+            terms of broker and <br/>
+            technology. And it is the<br/> 
+            smoothest app available <br/>
+            on any brokerage<br/> platform.</p>
+            </div>*/}
+
+<Carousel>
+  <Carousel.Item>
+  <div className={classes.Container}>
+  <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+  </div>
+
+  </Carousel.Item>
+  <Carousel.Item>
+  <div className={classes.Container}>
+  <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+  </div>
+  </Carousel.Item>
+  <Carousel.Item>
+  <div className={classes.Container}>
+  <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+            <div className={classes.box}>
+            <p className={classes.head}>Dhriti Chadda</p>
+            <p className={classes.text}>I'm working with Profitbag <br/>
+            since Jan-2017. All I can
+            say  <br/>that they are best in 
+            terms  <br/>of broker and 
+            technology.  <br/>And it is the 
+            smoothest  <br/>app available 
+            on any  <br/>brokerage platform.</p>
+            </div>
+  </div>
+  </Carousel.Item>
+</Carousel>
+            </div> 
+
+        // </div>
       );
     }
             
