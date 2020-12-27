b2e756708baeaec090f899ad146642bf1f2af788
Arnavjain1703 <arnavjainkht@gmail.com>
2020-12-27 14:28:21 +0530

    ..

--- frontend/src/Components/TradeWithUs/TradeWithUs.js
+++ frontend/src/Components/TradeWithUs/TradeWithUs.js
@@ -2,13 +2,37 @@ import React, { Component } from "react";
 import classes from './TradeWithUs.module.css';
 
 class TradeWithUs extends Component {
+  state=
+  {
+     value:0,
+     value2:0,
+     value3:0 
+    }
+  constructor(props)
+  {
+    super(props)
+    this.myRef = React.createRef();
+  }
+   componentDidMount()
+  {
+    
+    window.addEventListener('scroll', () => {
+      this.setState({value:window.pageYOffset / (document.body.offsetTop - window.innerHeight)});
+      this.setState({value2:this.state.value})
+      this.setState({value3:((this.state.value2))})
+      console.log(this.state.value3)
+    }, false);
 
+  }
+  
     render(){
-     
+      
       return (
-        <div className={classes.tradewithus}>
-        TradeWithUs
+        <div onScroll={this.onScroll}  className={classes.tradewithus}>
+
+        <div  style={{backgroundColor: "green" ,animationDelay:`${this.state.value3}`+"s" }}className={classes.box}></div>
         </div>
+
       );
     }
             
