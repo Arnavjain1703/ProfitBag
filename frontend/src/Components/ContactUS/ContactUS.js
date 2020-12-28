738332f6ca4086c4c0eeb378eb0d093a5fdae5c9
Rakshit <rakshitpanjwani@gmail.com>
2020-12-28 22:30:56 +0530

    added emailjs

--- frontend/src/Components/ContactUS/ContactUS.js
+++ frontend/src/Components/ContactUS/ContactUS.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from './ContactUs.module.css';
 import Aos from 'aos';
 import 'aos/dist/aos.css'
+import emailjs from "emailjs-com";
 
 class ContactUs extends Component {
 
@@ -25,11 +26,21 @@ class ContactUs extends Component {
     this.setState ( { [event.target.name] :event.target.value  } )
   }
 
-  handlesubmit = (event) => {
-    event.preventDefault();
+  handlesubmit = (e) => {
+    e.preventDefault();
     console.log(this.state.name)
     console.log(this.state.contact)
     console.log(this.state.message)
+
+    e.preventDefault();
+
+    emailjs.sendForm('service_dfg3xys', 'template_rl22rbh', e.target, 'user_COz5ok5wKikrQvGtbs0kd')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+        e.target.reset()
   }
 
     render(){
@@ -39,11 +50,11 @@ class ContactUs extends Component {
             <div className={classes.image} data-aos="fade-up"></div>
         <p  data-aos="fade-up" className={classes.heading}>Contact US</p>
         <p  data-aos="fade-up" className={classes.text}>Having any query about features, trials, pricing, need a demo, or anything else? Our team is ready to answer all your questions</p>
-        <form data-aos="fade-up" className={classes.form}>
+        <form data-aos="fade-up" className={classes.form} onSubmit={this.handlesubmit}>
           <input type="text" name="name" className={classes.fields} onChange={this.handlechangeall} placeholder="Your Name"></input><br/>
           <input type="number" name="contact" className={classes.fields} onChange={this.handlechangeall} placeholder="Contact Number"></input><br/>
           <textarea name="message" className={classes.textarea} onChange={this.handlechangeall} placeholder="Your Message"></textarea><br/>
-          <input type="submit" onClick={this.handlesubmit} className={classes.submitbtn} value="Send Message"></input><br/>
+          <input type="submit" className={classes.submitbtn} value="Send Message"></input><br/>
         </form>
         </div>
       );
