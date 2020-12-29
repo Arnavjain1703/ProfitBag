1f4cecffd3218b7499edbda83ad2be3c8faf6913
Rakshit <rakshitpanjwani@gmail.com>
2020-12-29 18:58:59 +0530

    added form validations

--- frontend/src/Components/ContactUS/ContactUS.js
+++ frontend/src/Components/ContactUS/ContactUS.js
@@ -26,6 +26,12 @@ class ContactUs extends Component {
     this.setState ( { [event.target.name] :event.target.value  } )
   }
 
+  maxLengthCheck = (object) => {
+    if (object.target.value.length > object.target.maxLength) {
+     object.target.value = object.target.value.slice(0, object.target.maxLength)
+      }
+    }
+
   handlesubmit = (e) => {
     e.preventDefault();
     console.log(this.state.name)
@@ -52,9 +58,14 @@ class ContactUs extends Component {
         <p className={classes.heading}>Contact US</p>
         <p className={classes.text}>Having any query about features, trials, pricing, need a demo, or anything else? Our team is ready to answer all your questions</p>
         <form className={classes.form} onSubmit={this.handlesubmit}>
-          <input type="text" name="name" className={classes.fields} onChange={this.handlechangeall} placeholder="Your Name"></input><br/>
-          <input type="number" name="contact" className={classes.fields} onChange={this.handlechangeall} placeholder="Contact Number"></input><br/>
-          <textarea name="message" className={classes.textarea} onChange={this.handlechangeall} placeholder="Your Message"></textarea><br/>
+          <input type="text" required name="name" className={classes.fields} onChange={this.handlechangeall} placeholder="Your Name"></input><br/>
+          {/* <input type="number" required name="contact" className={classes.fields} onChange={this.handlechangeall} placeholder="Contact Number"></input><br/> */}
+          
+          <input required name="contact" className={classes.fields} onChange={this.handlechangeall} placeholder="Contact Number"
+          pattern="[0-9]{10}" title="This is an error message"
+          ></input><br/>
+
+          <textarea name="message" required className={classes.textarea} onChange={this.handlechangeall} placeholder="Your Message"></textarea><br/>
           <input type="submit" className={classes.submitbtn} value="Send Message"></input><br/>
         </form>
         </div>
